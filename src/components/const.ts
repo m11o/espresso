db27665a12ccb4b81cf0e1a4b3e9d23cf99c9661
m11o <m11o@hey.jp>
2022-05-18 01:05:05 +0900

    定数を別ファイルで管理

---
+++ src/components/const.ts
@@ -0,0 +1,15 @@
+const SITE_TITLE = '首・肩・頭痛・産後骨盤矯正・骨格矯正といえばアンドライフ整骨院鍼灸院'
+const SITE_NAME = 'アンドライフ整骨院・鍼灸院'
+const ADDRESS = '福岡県糸島市高田５-1-16'
+const ZIP = '〒819-1102'
+const TEL = '092-332-2206'
+const TEL_WITHOUT_HYPHEN = TEL.replaceAll('-', '')
+
+export {
+  SITE_TITLE,
+  SITE_NAME,
+  ADDRESS,
+  ZIP,
+  TEL,
+  TEL_WITHOUT_HYPHEN
+}
