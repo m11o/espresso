5892982b16fa222e4aabb01cc825d5b6ca61e53b
m11o <m11o@hey.jp>
2022-05-18 23:09:40 +0900

    add sidemenu

---
+++ src/components/side_menu.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+const SideMenu: React.FC = () => {
+  return (
+    <aside id="side" className="" role="complementary">
+      <div className="side-nav sp-set">
+        <h2 className="side-nav-title">メニュー</h2>
+        <div className="menu-%e3%82%b5%e3%82%a4%e3%83%89%ef%bc%86%e3%83%95%e3%83%83%e3%82%bf%e3%83%bc-container">
+          <ul
+            id="menu-%e3%82%b5%e3%82%a4%e3%83%89%ef%bc%86%e3%83%95%e3%83%83%e3%82%bf%e3%83%bc"
+            className="menu"
+          >
+            <li
+              id="menu-item-1581"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1581"
+            >
+              <a href="https://www.ito-spo.com/staff/">スタッフ紹介</a>
+            </li>
+            <li
+              id="menu-item-1586"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1586"
+            >
+              <a href="https://www.ito-spo.com/menu/">メニュー</a>
+            </li>
+            <li
+              id="menu-item-1637"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1637"
+            >
+              <a href="https://www.ito-spo.com/facility/">院内紹介</a>
+            </li>
+            <li
+              id="menu-item-1583"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1583"
+            >
+              <a href="https://www.ito-spo.com/faq/">よくあるご質問</a>
+            </li>
+            <li
+              id="menu-item-1582"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1582"
+            >
+              <a href="https://www.ito-spo.com/access/">アクセス</a>
+            </li>
+            <li
+              id="menu-item-1578"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1578"
+            >
+              <a href="https://www.ito-spo.com/voices/">お客様の声</a>
+            </li>
+            <li
+              id="menu-item-1584"
+              className="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-483 current_page_item menu-item-1584"
+            >
+              <a href="https://www.ito-spo.com/contact/">
+                ご予約／お問い合わせ
+              </a>
+            </li>
+            <li
+              id="menu-item-1579"
+              className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1579"
+            >
+              <a href="https://www.ito-spo.com/sitemap/">サイトマップ</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="side-nav sp-set">
+        <h2 className="side-nav-title">症状別メニュー</h2>
+        <ul>
+          <li>
+            <a href="https://www.ito-spo.com/symptomscat/post-3650/">
+              頭痛専門
+            </a>
+          </li>
+          <li>
+            <a href="https://www.ito-spo.com/symptomscat/post-2792/">
+              初めての方へ
+            </a>
+          </li>
+          <li>
+            <a href="https://www.ito-spo.com/symptomscat/post-2607/">
+              交通事故専門治療
+            </a>
+          </li>
+          <li>
+            <a href="https://www.ito-spo.com/symptomscat/1679/">
+              頑固な腰痛でお困りな方へ
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  )
+}
+export default SideMenu
